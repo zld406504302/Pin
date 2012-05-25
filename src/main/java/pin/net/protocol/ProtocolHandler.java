0ede5da272698ca61bd96625cedfcb5843139a68
李中元 <lizhyvip@gmail.com>
2012-05-25 12:00:49 +0800

    添加ProtocolHandler注释

--- src/main/java/pin/net/protocol/ProtocolHandler.java
+++ src/main/java/pin/net/protocol/ProtocolHandler.java
@@ -5,7 +5,31 @@ import org.jboss.netty.channel.ChannelHandlerContext;
 import com.liteProto.LlpMessage;
 
 public interface ProtocolHandler {
+	/**
+	 * 预处理
+	 * 
+	 * @param ctx
+	 *            {@link ChannelHandlerContext} 上下文
+	 */
 	public void preHandle(ChannelHandlerContext ctx);
+
+	/**
+	 * 接收处理
+	 * 
+	 * @param ctx
+	 *            {@link ChannelHandlerContext} 上下文
+	 * @param msg
+	 *            接收到得消息
+	 */
 	public void handleReceived(ChannelHandlerContext ctx, LlpMessage msg);
+
+	/**
+	 * 发送处理
+	 * 
+	 * @param ctx
+	 *            {@link ChannelHandlerContext} 上下文
+	 * @throws Exception
+	 *             异常信息
+	 */
 	public void handleReply(ChannelHandlerContext ctx) throws Exception;
 }
