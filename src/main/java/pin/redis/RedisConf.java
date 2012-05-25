505dac47dca5941c2b1e31e9cafb3117a5e8af4e
李中元 <lizhyvip@gmail.com>
2012-05-25 19:15:59 +0800

    添加redis配置

---
+++ src/main/java/pin/redis/RedisConf.java
@@ -0,0 +1,32 @@
+package pin.redis;
+
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * redis 配置静态类
+ * @author zhongyuan
+ *
+ */
+public final class RedisConf {
+	public static final int MAX_ACTIVE = 100;
+	public static final int MAX_IDLE = 20;
+	public static final int MAX_WAIT = 1000;
+	public static final boolean TEST_ON_BORROW = true;
+	
+	/**
+	 * 私有构造函数
+	 */
+	private RedisConf() {
+		
+	}
+	
+	public static JedisPoolConfig getDefaultConf() {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxActive(RedisConf.MAX_ACTIVE);
+		config.setMaxIdle(RedisConf.MAX_IDLE);
+		config.setMaxWait(RedisConf.MAX_WAIT);
+		config.setTestOnBorrow(RedisConf.TEST_ON_BORROW);
+		
+		return config;
+	}
+}
