1bde1e11d2d94cfcab00e1aad1dd9309db1e0b02
李中元 <lizhyvip@gmail.com>
2012-08-13 14:44:43 +0800

    添加序列化接口

---
+++ src/main/java/pin/io/Serializable.java
@@ -0,0 +1,15 @@
+package pin.io;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhongyuan
+ * Date: 12-8-13
+ * Time: 下午2:41
+ */
+public interface Serializable {
+    /**
+     * 序列化为字符串
+     * @return 序列化后的字符串
+     */
+    String serialize();
+}
